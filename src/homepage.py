198bbd80108659767c0e0ea9e604ef56cb2d27ac
Brad Waterhouse <brad.waterhouse97@gmail.com>
2019-07-22 22:05:48 +0100

    imports unittest

--- src/homepage.py
+++ src/homepage.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
+import unittest
 
-def main():
+
+class TestStringMethods(unittest.TestCase):
+
     browser = webdriver.Chrome("/Users/bradWaterhouse/Downloads/chromedriver")
 
     chrome_options = webdriver.ChromeOptions()
@@ -10,12 +13,22 @@ def main():
         "profile.default_content_setting_values.notifications": 2
     })
 
+    def test_pick_of_the_day(self):
+        browser = webdriver.Chrome("/Users/bradWaterhouse/Downloads/chromedriver")
+        browser.get('https://www.bunches.co.uk')
+        self.assertTrue(browser.find_element_by_xpath('/html/body/div[2]/div/div[2]/div[1]/div[1]/a/img').is_displayed())
 
-    browser.get('https://www.bunches.co.uk')
-    if browser.find_element_by_xpath('/html/body/div[2]/div/div[2]/div[1]/div[1]/a/img').is_displayed():
-        print('Pick of the day image exists')
-        return True
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+
+        with self.assertRaises(TypeError):
+            s.split(2)
 
 
 if __name__ == '__main__':
-    main()
+    unittest.main()
